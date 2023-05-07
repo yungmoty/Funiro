a1a8ca2d1b42ee8a66bae051b148fcaafdfb4f8f
yungmoty <ycatbiy64@gmail.com>
2023-05-07 18:34:21 +0300

    Changed the swapper adaptive and added an interesting header
    Added a header design using the IntersectionObserver method

--- js/common.js
+++ js/common.js
@@ -166,9 +166,23 @@ window.addEventListener('DOMContentLoaded', function () {
 			},
 		})
 	};
-
-
 	//\\swiper//\\
+
+	//\\header\\//
+	const headerElement = document.querySelector('.header');
+
+	const callback = function (entries, observer) {
+		if (entries[0].isIntersecting) {
+			headerElement.classList.remove('_scroll');
+		} else {
+			headerElement.classList.add('_scroll');
+		}
+	};
+
+	const headerObserver = new IntersectionObserver(callback);
+
+	headerObserver.observe(headerElement)
+	//\\header//\\
 });
 
 
