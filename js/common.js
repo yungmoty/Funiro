3f889a2cd6b2a482086d0cffac9e362bb10f1690
yungmoty <ycatbiy64@gmail.com>
2023-05-08 15:42:13 +0300

    Loading from JSON
    Added ability to load other product cards from JSON

--- js/common.js
+++ js/common.js
@@ -49,6 +49,14 @@ window.addEventListener('DOMContentLoaded', function () {
 		} else if (!targetElement.closest('.search-form') && document.querySelector('.search-form._active'))
 			document.querySelector('.search-form').classList.remove('_active');
 		//\\search//\\
+
+		//\\More Products\\//
+		if (targetElement.classList.contains('products__more')) {
+			getProducts(targetElement);
+			e.preventDefault();
+
+		}
+		//\\More Products//\\
 	}
 
 	//\\accordion\\//
@@ -186,9 +194,120 @@ window.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
+// Remove Classes
 function removeClasses(element, className) {
 	for (let index = 0; index < element.length; index++) {
 		element[index].classList.remove(className);
 	}
+}
+
+// Load More Products
+async function getProducts(button) {
+	if (!button.classList.contains('_hold')) {
+		button.classList.add('_hold');
+		const file = 'json/products.json';
+		let response = await fetch(file, {
+			method: 'GET'
+		});
+
+		if (response.ok) {
+			let result = await response.json();
+			loadProducts(result);
+			button.classList.remove('_hold');
+			button.remove();
+		} else {
+			alert('Error');
+		}
+	}
+}
+
+// JSON load
+function loadProducts(data) {
+	const productsItems = document.querySelector('.products__items');
+
+	data.products.forEach(item => {
+		const productId = item.id;
+		const productUrl = item.url;
+		const productImage = item.image;
+		const productTitle = item.title;
+		const productText = item.text;
+		const productPrice = item.price;
+		const productPriceOld = item.priceOld;
+		const productShareUrl = item.shareUrl;
+		const productLikeUrl = item.likeUrl;
+		const productLabels = item.labels;
+
+		let productTemplateStart = `<article data-product-id="${productId}" class="products__item item-product">`;
+		let productTemplateEnd = `</article>`;
+
+		let productTemplateLabels = '';
+		if (productLabels) {
+			let productTemplateLabelsStart = `<div class="item-product__labels">`;
+			let productTemplateLabelsEnd = `</div>`;
+			let productTemplateLabelsContent = '';
+
+			productLabels.forEach(labelItem => {
+				productTemplateLabelsContent += `<div class="item-product__label item-product__label_${labelItem.type}">${labelItem.value}</div>`
+			})
+
+			productTemplateLabels += productTemplateLabelsStart;
+			productTemplateLabels += productTemplateLabelsContent;
+			productTemplateLabels += productTemplateLabelsEnd;
+		}
+
+		let productTemplateImage = `
+		<a href="${productUrl}" class="item-product__image _ibg">
+			<img src="images/products/${productImage}" alt="${productTitle}">
+		</a>
+		`;
+
+		let productTemplateBodyStart = `<div class="item-product__body">`;
+		let productTemplateBodyEnd = `</div>`;
+
+		let productTemplateContent = `
+		<div class="item-product__content">
+			<h5 class="item-product__title">${productTitle}</h5>
+			<div class="item-product__text">${productText}</div>
+		</div>
+		`;
+
+		let productTemplatePrices = '';
+		let productTemplatePricesStart = `<div class="item-product__prices">`;
+		let productTemplatePricesCurrent = `<div class="item-product__price">${productPrice}</div>`;
+		let productTemplatePricesOld = `<div class="item-product__price item-product__price_old">Rp ${productPriceOld}</div>`;
+		let productTemplatePricesEnd = `</div>`;
+
+		productTemplatePrices = productTemplatePricesStart;
+		productTemplatePrices += productTemplatePricesCurrent;
+		if (productPriceOld) {
+			productTemplatePrices += productTemplatePricesOld;
+		}
+		productTemplatePrices += productTemplatePricesEnd;
+
+		let productTemplateActions = `
+		<div class="item-product__actions actions">
+			<div class="actions__body">
+				<a href="" class="actions__button btn btn_white">Add to cart</a>
+				<a href="${productShareUrl}" class="actions__link"><span class="_icon-share"></span>Share</a>
+				<a href="${productLikeUrl}" class="actions__link "><span class="_icon-favorite"></span>Like</a>
+			</div>
+		</div>
+		`;
+
+		let productTemplateBody = '';
+		productTemplateBody += productTemplateBodyStart;
+		productTemplateBody += productTemplateContent;
+		productTemplateBody += productTemplatePrices;
+		productTemplateBody += productTemplateActions;
+		productTemplateBody += productTemplateBodyEnd;
+
+		let productTemplate = '';
+		productTemplate += productTemplateStart;
+		productTemplate += productTemplateLabels;
+		productTemplate += productTemplateImage;
+		productTemplate += productTemplateBody;
+		productTemplate += productTemplateEnd;
+
+		productsItems.insertAdjacentHTML('beforeend', productTemplate);
+	})
 }
